59477723465318ee540863075a92cf981bbe34ee
AshrakatAhmed18 <Ashrakatahmed181999@gmail.com>
2023-05-10 22:16:38 +0300

    first task

---
+++ Lab1/Observer DP/Classes/Football.cs
@@ -0,0 +1,47 @@
+﻿using Lab1.Observer_DP.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1.Observer_DP.Classes
+{
+    internal class Football:IBall
+    {
+        public List<IObserver> Observers { get; set; }
+        private Position _ballPosition;
+        public Position MyPosition
+        {
+            get => _ballPosition;
+            set
+            {
+                _ballPosition = value;
+                NotifyObservers();
+            }
+        }
+        public Football()
+        {
+            _ballPosition = new Position();
+            Observers = new List<IObserver>();
+        }
+     
+        public void AttachObserver(IObserver observer)
+        {
+            Observers.Add(observer);
+        }
+
+        public void DetachObserver(IObserver observer)
+        {
+            Observers.Remove(observer);
+        }
+
+        public void NotifyObservers()
+        {
+            foreach (var observer in Observers)
+            {
+                observer.Update(_ballPosition);
+            }
+        }
+    }
+}
---
+++ Lab1/Program.cs
@@ -0,0 +1,24 @@
+﻿using Lab1.Observer_DP.Classes;
+
+namespace Lab1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var FootballMatch = new Football();
+
+            var player1 = new Player();
+            var player2 = new Player();
+            var player3 = new Player();
+            var referee = new Referee();
+
+            FootballMatch.AttachObserver(player1);
+            FootballMatch.AttachObserver(player2);
+            FootballMatch.AttachObserver(player3);
+            FootballMatch.AttachObserver(referee);
+
+            FootballMatch.MyPosition = new Position() { X = 0, Y = 0, Z = 0 };
+        }
+    }
+}
